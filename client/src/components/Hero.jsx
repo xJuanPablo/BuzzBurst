27377350fe76d15a12b4f1059e3b5466a7a6b81a
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-12-02 11:32:25 -0600

    Finished starting styling for Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import HeroImg from '../assets/HeroImg.png'
 
 function Hero() {
   return (
     <>
-    <div className='home-container'>
+    <div className='hero-container'>
+      <div className='tag-container'>
+        <h3>
+        Unleash the power of connection with BuzzBurst
+        </h3>
+        <p>
+        Enjoy instant, personalized conversations with a user-friendly interface. Stay connected effortlessly with friends and family for a dynamic and enjoyable texting experience.
+        </p>
+      </div>
+      <img src={HeroImg} alt="Hero" className='hero-img' />
     </div>
     </>
   )
