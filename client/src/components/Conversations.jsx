71ba17c02f72213cc1825472cc56204d972d6e56
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-12-03 16:33:14 -0600

    Finished Side Bar

--- client/src/components/Conversations.jsx
+++ client/src/components/Conversations.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { useConversations } from '../Context/ConversarionProvider';
 
 function Conversations() {
+
+  const {conversations, selectedConversationIndex} = useConversations()
+
   return (
-    <div>Conversations</div>
+    <ListGroup varient="flush">
+    {conversations.map((conversation, index) => (
+      <ListGroup.Item key={index}
+      action
+      onClick={() => selectedConversationIndex(index)}
+      active={conversation.selected}
+      >
+        {conversation.recipients.map(r => r.name).join(', ')}
+      </ListGroup.Item>
+    ))}
+  </ListGroup>
   )
 }
 
